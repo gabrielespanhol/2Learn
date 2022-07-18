afd775c63dfef2e2c47a0c906c809d15f6539dc9
gabrielespanhol <gabryel.ehs@gmail.com>
2022-07-18 13:22:05 -0300

    commit

--- lib/constant.dart
+++ lib/constant.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 const KPrimaryColor = Color(0xFFFFC200);
 const kPrimaryLightColor = Color(0xFFF1E6FF);
+const KPrimary = Color.fromARGB(255, 39, 39, 39);
 const KTextcolor = Color.fromARGB(255, 39, 39, 39);
 const KTextcolorLight = Color.fromARGB(255, 143, 142, 142);
 const KTextButtoncolor = Color.fromARGB(255, 255, 255, 255);
