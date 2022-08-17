969b157994b1c04dc7bc5f61cb2df4166833a97f
gabrielespanhol <gabryel.ehs@gmail.com>
2022-08-17 09:57:44 -0300

    base_teachers_screens

---
+++ lib/Screens/TeacherScreens/RegisterCourses/web_register_courses_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WebRegisterCourses extends StatelessWidget {
+  const WebRegisterCourses({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Size size = MediaQuery.of(context).size;
+    return Column(
+      children: const [],
+    );
+  }
+}
